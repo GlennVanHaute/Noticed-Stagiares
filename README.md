72e9f55d417bc37434e2ab255a5ee731deafad3f
glenn <glennvanhaute@gmail.com>
2016-02-24 09:43:30 +0100

    gulp added

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# Noticed-Stagiares
+# NOTICED GULP
+
+## INSTALLATION
+
+### First time only
+
+Make sure you have nodeJS installed.
+http://nodejs.org/
+
+After you have installed nodeJS, you can go and install gulp globally with following command:
+
+```(sudo) npm install gulp -g```
+
+Also install Bower globally:
+
+```(sudo) npm install bower -g```
+
+
+### Normal installation
+
+Download this repository and extract the contents into your theme folder.
+
+*IMPORTANT: *Move the contents of the extracted folder (typically 'noticed-gulp-master') into the parent folder (the root of your theme).
+
+Now, open your terminal and go to your used theme folder and run following command to install all necessary node modules referenced in package.json file:
+
+```(sudo) npm install```
+
+Also get the bower components for susy(grid) and breakpoint-sass from the bower.json file.
+
+```(sudo) bower install```
+
+All your sass files will be compiled in the assets/css folder, so don't forget to add this to your theme settings.
+
+## USING GULP
+
+Tell Gulp to watch for changes
+
+```gulp watch```
---
+++ gulpfile.js
@@ -0,0 +1,67 @@
+var gulp = require('gulp');
+	concat = require('gulp-concat');
+	sass = require('gulp-sass');
+	cssGlobbing = require('gulp-css-globbing');
+	livereload = require('gulp-livereload');
+	plumber = require('gulp-plumber');
+	sourcemaps = require('gulp-sourcemaps');
+	notify = require("gulp-notify");
+	autoprefixer = require('gulp-autoprefixer');
+	cache = require('gulp-cache'),
+	imagemin = require('gulp-imagemin'),
+	cssnano = require('gulp-cssnano'),
+	pngquant = require('imagemin-pngquant');
+	rename = require('gulp-rename');
+
+
+function errorAlertSass(error){
+	notify.onError({title: "SCSS Error", message: "Check your terminal", sound: "Sosumi"})(error); //Error Notification
+	console.log(error.toString());//Prints Error to Console
+	this.emit("end"); //End function
+};
+function errorAlertJS(error){
+	notify.onError({title: "JS Error", message: "Check your terminal", sound: "Sosumi"})(error); //Error Notification
+	console.log(error.toString());//Prints Error to Console
+	this.emit("end"); //End function
+};
+
+gulp.task('sass', function(){
+  	return gulp.src('src/scss/**/*.scss')
+	.pipe(cssGlobbing({extensions: ['.scss']}))
+	.pipe(plumber({errorHandler: errorAlertSass}))
+	.pipe(sourcemaps.init({loadMaps: true}))
+    	.pipe(sass()) // Using gulp-sass
+    	.pipe(autoprefixer())
+    	.pipe(gulp.dest('dist/css'))
+    	.pipe(rename({suffix: '.min'}))
+    	.pipe(cssnano())
+    	.pipe(sourcemaps.write('../maps'))
+    	.pipe(gulp.dest('dist/css'))
+    	.pipe(livereload());
+});
+
+gulp.task('scripts', function () {
+	return gulp.src("src/js/*.js")
+	.pipe(plumber({errorHandler: errorAlertJS}))
+	.pipe(sourcemaps.init({loadMaps: true}))
+	.pipe(concat('scripts.js'))
+	.pipe(gulp.dest('dist/js/'))
+	.pipe(rename({suffix: '.min'}))
+	.pipe(uglify())
+	.pipe(sourcemaps.write('../maps'))
+	.pipe(gulp.dest('dist/js/'))
+	.pipe(livereload());
+});
+
+gulp.task('images', function() {
+	return gulp.src('src/img/*.{png,gif,jpg,jpeg,svg}')
+	.pipe(cache(imagemin({ optimizationLevel: 5, progressive: true, interlaced: true, use: [pngquant()] })))
+	.pipe(gulp.dest('dist/img'))
+});
+
+gulp.task('watch', function(){
+	livereload.listen();
+  	gulp.watch('src/scss/**/*.scss', ['sass']);
+  	gulp.watch('src/js/*.js', ['scripts']);
+  	gulp.watch('src/img/*{png,gif,jpg,jpeg,svg}', ['images']);
+});
