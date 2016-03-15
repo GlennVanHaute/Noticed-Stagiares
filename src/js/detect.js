0af722fdae3c9c77487cb142e86921f1862d58f2
Glenn <glenn@MacBook-Pro-van-Karlo.local>
2016-03-15 15:24:12 +0100

    internet popup

--- src/js/detect.js
+++ src/js/detect.js
@@ -4,6 +4,7 @@ var IEversion = detectIE();
 if (IEversion !== false) {
 
         $(".logo-static").css("display", "block");
+        
         if (IEversion <12){
           location.href = "#popup1";
         }
